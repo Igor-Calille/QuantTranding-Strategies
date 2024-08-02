1948f5a8a464e0f89bfc45d7555cf16401dc3176
igorm <igormassoncalille123@gmail.com>
2024-08-02 16:38:09 -0300

    Release: adding files and yfinance

---
+++ IndicadoresTecnicos.py
@@ -0,0 +1,23 @@
+
+class IndicadoresTecnicos():
+    def __init__(self):
+        pass
+
+    def computar_media_movel(self, data, periodo):
+        return data.rolling(window=periodo).mean()
+    
+    def computar_RSI(self, data, periodo=14):
+        delta = data.diff().dropna()
+        gain = (delta.where(delta>0, 0)).rolling(window=periodo).mean()
+        loss = (-delta.where(delta<0, 0)).roling(window=periodo).mean()
+        RS = gain/loss
+
+        return 100 - (100/(1+RS))
+    
+    def computar_Bollinger_Bands(self, data, periodo=20, desvio_padrao=2):
+        rolling_mean = data.rolling(window=periodo).mean()
+        rolling_std = data.rolling(window=periodo).std()
+        bollinger_high = rolling_mean + (desvio_padrao * rolling_std)
+        bollinger_low = rolling_mean - (desvio_padrao * rolling_std)
+
+        return bollinger_high, bollinger_low
---
+++ StockIntegration.py
@@ -0,0 +1,40 @@
+import yfinance as yf
+from datetime import datetime
+
+class yfinance():
+    def __init__(self):
+        pass
+
+    def get_data_yfinance(self, symbol, start_date):
+        
+        data = yf.download(symbol, start=start_date)
+
+        if data.empty:
+            print("Stocks nao encontrados")
+            return None
+        else:
+            print(f'Dados obtidos, total de linhas: {len(data)}')
+
+        # Formatar dados
+        data.reset_index(inplace=True)
+        data = data.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Volume': 'volume', 'Adj Close': 'adj_close'})
+
+        return data
+
+    def get_data_yfinance_H(symbol):
+        data = yf.download(symbol, interval='1h')
+
+        data_H = data.resample('2H').agg({
+            'Open': 'first',
+            'High': 'max',
+            'Low': 'min',
+            'Close': 'last',
+            'Volume': 'sum'
+        }) 
+
+        data_H = data_H.dropna()
+
+        data_H.reset_index(inplace=True)
+        data_H = data_H.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Volume': 'volume', 'Adj Close': 'adj_close'})
+
+        return data_H
